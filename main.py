1cec64fa0dcd2ede251ff85911ae9ad0e813a9b7
iDavidus01 <145462616+iDavidus01@users.noreply.github.com>
2023-12-24 15:33:33 +0100

    user

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-def handle_add_command(args):
+user = input("Login: ")
+
+
+def add_command(args):
     if len(args) >= 2:
         command = args[0]
         content = ' '.join(args[1:])
         with open('data.py', 'a') as file:
-            file.write(f'\n{{"command": "{command}", "content": "{content}"}}')
+            file.write(f'\n{{"command": "{command}", "content": "{content}", "owner": "{user}"}}')
         print("OK")
     else:
         print("Error")
 
-def handle_cat_command(args):
+
+def cat_command(args):
     if args:
         name = args[0]
         found = False
         with open('data.py', 'r') as file:
             for line in file:
-                if f'"command": "{name}"' in line:
+                if f'"command": "{name}"' in line and f'"owner": "{user}"' in line:
                     content = line.split('"content": "')[1].split('"')[0]
                     print(content)
                     found = True
                     break
         if not found:
             print("Error")
+
 
 while True:
     command = input("#> ").split(' ')
@@ -29,6 +34,6 @@ while True:
         case ["quit"]:
             break
         case ["add", "note", *args]:
-            handle_add_command(args)
+            add_command(args)
         case ["cat", *args]:
-            handle_cat_command(args)
+            cat_command(args)
