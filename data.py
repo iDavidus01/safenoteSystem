71ba02ce904d4e66eb967088d6d62505b52cbd8c
iDavidus01 <145462616+iDavidus01@users.noreply.github.com>
2023-12-28 16:49:04 +0100

    notatka

---
+++ data.py
@@ -0,0 +1,4 @@
+
+{"command": "laczenie", "content": "A=T lub A=U jesli RNA G=C", "owner": "David", "access": "u-r"}
+{"command": "DNA", "content": "DNA wystepuje w jadrze komorkowym, mitochondriach itp.", "owner": "David", "access": "u-r"}
+{"command": "???", "content": "nic", "owner": "David", "access": "u-n"}
