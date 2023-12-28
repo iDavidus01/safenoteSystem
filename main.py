aa4229b7f3cbeffe7106d29c186c19f87c75436f
iDavidus01 <145462616+iDavidus01@users.noreply.github.com>
2023-12-28 13:43:37 +0100

    editAccess

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ user = input("Login: ")
 
 
 def add_command(args):
-    if len(args) >= 2:
+    if len(args) >= 3:
         command = args[0]
-        content = ' '.join(args[1:])
+        access_type = args[-1]
+        content = ' '.join(args[1:-1])
         with open('data.py', 'a') as file:
-            file.write(f'\n{{"command": "{command}", "content": "{content}", "owner": "{user}"}}')
+            file.write(f'\n{{"command": "{command}", "content": "{content}", "owner": "{user}", "access": "{access_type}"}}')
         print("OK")
     else:
         print("Error")
@@ -18,12 +19,40 @@ def cat_command(args):
         found = False
         with open('data.py', 'r') as file:
             for line in file:
-                if f'"command": "{name}"' in line and f'"owner": "{user}"' in line:
-                    content = line.split('"content": "')[1].split('"')[0]
-                    print(content)
-                    found = True
-                    break
+                if f'"command": "{name}"' in line and (f'"owner": "{user}"' in line or f'"access": "u-r"' in line):
+                    access_type = line.split('"access": "')[1].split('"')[0]
+                    if access_type in ["u-n", "u-r", "u-x"]:
+                        content = line.split('"content": "')[1].split('"')[0]
+                        print(content)
+                        found = True
+                        break
+                    else:
+                        print("Nie masz do tego uprawnien")
+                        found = True
+                        break
         if not found:
+            print("Error")
+
+
+def edit_command(args):
+    if args:
+        name = args[0]
+        found = False
+        with open('data.py', 'r') as file:
+            lines = file.readlines()
+        with open('data.py', 'w') as file:
+            for line in lines:
+                if f'"command": "{name}"' in line and f'"owner": "{user}"' in line:
+                    access_type = line.split('"access": "')[1].split('"')[0]
+                    if access_type in ["u-n", "u-x", "u-r"]:
+                        content = line.split('"content": "')[1].split('"')[0]
+                        new_content = input(f"Napisz nowa tresc '{name}': ")
+                        line = line.replace(content, new_content)
+                        found = True
+                file.write(line)
+        if found:
+            print("OK")
+        else:
             print("Error")
 
 
@@ -37,3 +66,5 @@ while True:
             add_command(args)
         case ["cat", *args]:
             cat_command(args)
+        case ["edit", "note", *args]:
+            edit_command(args)
